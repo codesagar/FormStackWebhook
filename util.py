bc43097a09bb8bd1cde35ffcea2ba29f9c9aa7ae
Sagar Patel <sagarpatel.exe@gmail.com>
2016-02-15 03:08:30 +0530

    redundant

--- util.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-def constant_time_compare(val1, val2):
-    if len(val1) != len(val2):
-        return False
-    result = 0
-    for x, y in zip(val1, val2):
-        result |= ord(x) ^ ord(y)
-    return result == 0
